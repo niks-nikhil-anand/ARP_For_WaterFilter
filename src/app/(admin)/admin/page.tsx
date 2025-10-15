f3d8e1546f430fad5709d074feebaab2dbe0dedf
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-10-15 23:38:50 +0530

    Fix build error

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import {
   LineChart,

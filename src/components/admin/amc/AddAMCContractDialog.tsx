1dafd7c4f866e219a78105e84b469a9ba42b2863
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-11-29 21:19:14 +0530

    style: add selected state styling to dropdown list items in AMC contract dialog.

--- src/components/admin/amc/AddAMCContractDialog.tsx
+++ src/components/admin/amc/AddAMCContractDialog.tsx
@@ -309,7 +309,7 @@ export const AddAMCContractDialog = ({
                                         setAddForm(prev => ({ ...prev, customerId: customer.id.toString() }))
                                         setCustomerOpen(false)
                                       }}
-                                      className="cursor-pointer py-3"
+                                      className="cursor-pointer py-3 data-[selected='true']:bg-blue-50 data-[selected='true']:text-blue-700 dark:data-[selected='true']:bg-blue-900/20 dark:data-[selected='true']:text-blue-300"
                                     >
                                       <Check
                                         className={`mr-2 h-4 w-4 shrink-0 ${
@@ -408,7 +408,7 @@ export const AddAMCContractDialog = ({
                                         setAddForm(prev => ({ ...prev, agentId: agent.id.toString() }))
                                         setAgentOpen(false)
                                       }}
-                                      className="cursor-pointer py-3"
+                                      className="cursor-pointer py-3 data-[selected='true']:bg-blue-50 data-[selected='true']:text-blue-700 dark:data-[selected='true']:bg-blue-900/20 dark:data-[selected='true']:text-blue-300"
                                     >
                                       <Check
                                         className={`mr-2 h-4 w-4 ${
@@ -509,7 +509,7 @@ export const AddAMCContractDialog = ({
                                       setAddForm(prev => ({ ...prev, productId: product.id.toString() }))
                                       setProductOpen(false)
                                     }}
-                                    className="cursor-pointer py-3"
+                                    className="cursor-pointer py-3 data-[selected='true']:bg-blue-50 data-[selected='true']:text-blue-700 dark:data-[selected='true']:bg-blue-900/20 dark:data-[selected='true']:text-blue-300"
                                   >
                                     <Check
                                       className={`mr-2 h-4 w-4 ${
